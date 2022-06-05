e018d5011b76375df599fb7c0e3ac3d282bb2077
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-05 07:14:50 +0300

    rf:modified user profile and user updateprofile page

--- insta/urls.py
+++ insta/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, re_path
 urlpatterns = [
     path('', views.homepage, name='landing'),
     path('profile/<int:profileId>', views.profile, name='profile'),
-    path('user/update/profile', views.update_profile, name='updateprofile'),
+    path('new/profile', views.update_profile, name='update_profile'),
 ]
