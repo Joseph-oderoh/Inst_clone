c11d3184e9bf6920ab06f61c6635e20835fc2ee3
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-05 16:05:14 +0300

    st:modified template style and add search form

--- insta/urls.py
+++ insta/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 
 urlpatterns = [
     path('', views.homepage, name='landing'),
+    path('search/', views.search_results, name='search_results'),
     path('user/<int:user_id>', views.profile, name='profile'),
     path('user/add/image', views.add_image, name='addimage'),
     path('user/update/profile', views.update_profile, name='updateprofile'),
